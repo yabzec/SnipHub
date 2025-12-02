ca20a767d06bfafe786f704d3009db7b2155d2e4
Matteo Colpo <colpo@esteco.com>
2025-12-02 10:26:14 +0100

    feat: wrangler file creation script for cloudlfare build

---
+++ backend/utils/createWrangler.cjs
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const wranglerPath = path.join(process.cwd(), 'wrangler-test.jsonc');
+const wranglerConfigObject = {
+    "$schema": "node_modules/wrangler/config-schema.json",
+    "name": "sniphub_backend",
+    "main": "src/index.ts",
+    "compatibility_date": "2025-11-26",
+    "vars": {
+        "FRONTEND_BASE_URL": process.env.FRONTEND_BASE_URL,
+        "NO_REPLY_EMAIL": process.env.NO_REPLY_EMAIL,
+        "ADMIN_EMAIL": process.env.ADMIN_EMAIL,
+    },
+    "triggers": {
+        "crons": [
+            "0 0 * * *"
+        ]
+    },
+    "d1_databases": [
+        {
+            "binding": process.env.DATABASE_NAME,
+            "database_name": process.env.DATABASE_NAME,
+            "database_id": process.env.DATABASE_ID,
+            "remote": true
+        }
+    ]
+};
+
+fs.writeFileSync(wranglerPath, JSON.stringify(wranglerConfigObject));
